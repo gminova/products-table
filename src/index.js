2d4a32bf6678317029f115d2aba393fdf2ca661f
Gigi Minova <minova.gigi@gmail.com>
2019-12-02 22:25:33 +0000

    tidy up

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import data from "./model/data";
+import Table from "./components/Table";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Table data={data} />, document.getElementById("root"));
